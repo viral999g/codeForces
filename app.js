b0e0f8d8bbe84d8bb945b20f59550f405443a569
Viral Gandhi <viral999g@gmail.com>
2021-04-10 20:00:02 +0530

    Basic Route

---
+++ app.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+})
+
+app.listen(PORT)
